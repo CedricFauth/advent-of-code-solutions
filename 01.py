69c2e8a5be5e270e8c587def76cc5985b3874c3c
Cedric <cedricfauth@yahoo.de>
2020-12-03 11:42:27 +0100

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,13 @@
+# 1
+for i in arr:
+  for j in arr:
+    if i+j == 2020:
+      print(i,j,i+j,i*j)
+
+# 2
+for i in arr:
+  for j in arr:
+    for y in arr:
+      if i+j+y == 2020:
+        print(i,j,y,i+j+y,i*j*y)
+
